0ec548f830bd921ba151e7c9cc7e348219dd97c4
Raul Gavris <ra.gavris@gmail.com>
2021-11-19 15:03:30 +0000

    errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,18 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
-// import 'select2/dist/css/select2.min'
-// import 'jquery-bar-rating/dist/themes/css-stars';
-// import 'aos/dist/aos';
-// import AOS from 'aos';
-// import { initTyped } from './plugins/init_typedjs';
-// import { initSelect2 } from './plugins/init_select2';
-// import { initStarRating } from './plugins/init_star_rating';
 
 
 document.addEventListener('turbolinks:load', () => {
-  //initSelect2();
-  //initTyped();
-  //AOS.init();
-  //initStarRating();
+
 });
