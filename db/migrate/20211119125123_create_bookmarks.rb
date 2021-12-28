02d84af3dfcf5ef6271b799fe92e935a7d45fce4
Raul Gavris <ra.gavris@gmail.com>
2021-12-28 11:40:13 +0000

    renamed the migration file

--- db/migrate/20211119125123_create_bookmarks.rb
+++ db/migrate/20211119125123_create_bookmarks.rb
@@ -1,4 +1,4 @@
-class CreateBookmark < ActiveRecord::Migration[6.1]
+class CreateBookmarks < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarks do |t|
       t.references :movie, null: false, foreign_key: true
